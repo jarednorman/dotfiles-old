34e850f2691b5cb5c7b80c84a75bbb1a1f469362
Jared Norman <jared@freerunningtechnologies.com>
2014-05-08 09:33:26 -0700

    Add guard configuration
    Change-Id: If93078f070febdf8e44e1c5e4448abd9fa904dc2

---
+++ guard/.guard.rb
@@ -0,0 +1,9 @@
+notification :tmux,
+  display_message: true,
+  timeout: 5, # in seconds
+  default_message_format: '%s >> %s',
+  # the first %s will show the title, the second the message
+  # Alternately you can also configure *success_message_format*,
+  # *pending_message_format*, *failed_message_format*
+  line_separator: ' > ', # since we are single line we need a separator
+  color_location: 'status-left-bg' # to customize which tmux element will change color
