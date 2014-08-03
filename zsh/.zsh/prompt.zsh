bac0012a84b0bdf2c99069a78413747030a6ed91
Jared Norman <jared@freerunningtechnologies.com>
2014-08-03 00:12:34 -0700

    More nice promptiness

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,11 +1,19 @@
 setopt promptsubst
 
+local suspended_jobs='%(1j.%j .)'
+local remote_user_host='${SSH_TTY:%n@%m ]}'
 local root_prompt='❯root❯'
 local user_prompt='❯❯❯'
 
-export PROMPT="
+export PROMPT="${suspended_jobs}${remote_user_host}
 %(!.${root_prompt}.${user_prompt}) "
 
-local git_current_branch='$(current_branch)'
+function parse_git_dirty {
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "⚡"
+}
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+}
+local git_current_branch='$(parse_git_branch)'
 
 export RPROMPT="${git_current_branch}"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,7 +1,7 @@
 autoload -U compinit promptinit
 compinit
 
-source .zsh/aliases/*.sh
-source .zsh/prompt.zsh
+source ~/.zsh/aliases/*.sh
+source ~/.zsh/prompt.zsh
 
 [ -f ~/.bundler-exec.sh ] && source ~/.bundler-exec.sh
