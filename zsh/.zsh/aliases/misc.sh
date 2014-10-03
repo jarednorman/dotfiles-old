8857fd2c0bc83a0a953f554255ccd006ef5a4e07
Jared Norman <jared@freerunningtechnologies.com>
2014-10-02 19:18:11 -0700

    Some crap

--- zsh/.zsh/aliases/misc.sh
+++ zsh/.zsh/aliases/misc.sh
@@ -2,5 +2,6 @@ alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-
 alias weechat="ssh jarednorman@gattsuman -t \"tmux attach\""
 alias tms="tmux attach -t"
 alias tm='tmux switch -t "`tmux list-sessions | cut -d':' -f1 | fzy`"'
+alias love=/Applications/love.app/Contents/MacOS/love
 
 f(){ ag --nocolor -l -g "$1" "${2:-.}"  }
