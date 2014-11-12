a54c8a5ec482485deb954fe67d3178215eae4ba6
Jared Norman <jared@freerunningtechnologies.com>
2014-11-12 09:30:56 -0800

    Fix weechat alias
    Change-Id: Idc140a4d20cbe80d0e8bbeb00fdd027ee51ba47c

--- zsh/.zsh/aliases/misc.sh
+++ zsh/.zsh/aliases/misc.sh
@@ -1,5 +1,5 @@
 alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
-alias weechat="ssh jarednorman@gattsuman -t \"tmux attach\""
+alias weechat="ssh boxer -t \"tmux attach\""
 alias tms="tmux attach -t"
 alias tm='tmux switch -t "`tmux list-sessions | cut -d':' -f1 | fzy`"'
 alias love=/Applications/love.app/Contents/MacOS/love
