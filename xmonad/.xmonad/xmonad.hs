787953e143e9500f1a696ec1a46f78f9d5420cc3
Jared Norman <jared@freerunningtechnologies.com>
2015-03-09 10:32:18 -0700

    Better for giger screens

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -4,8 +4,11 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
 
-myLayoutHook = avoidStruts (tall ||| noBorders Full)
-               where tall = Tall 1 (1/100) (1/2)
+myLayoutHook = avoidStruts (tall ||| twoTall ||| threeTall ||| noBorders threeTall ||| noBorders Full)
+               where
+                  tall      = Tall 1 (1/100) (1/2)
+                  twoTall   = Tall 2 (1/100) (1/2)
+                  threeTall = Tall 3 (1/100) (1/2)
 
 main = xmonad defaultConfig
               { manageHook         = manageDocks <+> manageHook defaultConfig
