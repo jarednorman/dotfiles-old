6c3c5a03cc1fd629bd2e08cbb6d9b0e1fbe1e345
Jared Norman <jared@freerunningtechnologies.com>
2015-03-09 11:15:17 -0700

    Make substring search work under linux

--- zsh/.zsh/aliases/history.sh
+++ zsh/.zsh/aliases/history.sh
@@ -1,3 +1,5 @@
-source ~/.zsh/vendor/zsh-history-substring-search.zsh
+source $HOME/.zsh/vendor/zsh-history-substring-search.zsh
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
