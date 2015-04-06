71705129d07424985aff1c4bf051a18bfb48e2bc
Jared Norman <jared@freerunningtechnologies.com>
2015-04-05 17:42:34 -0700

    Good irc alias

--- zsh/.zsh/aliases/misc.sh
+++ zsh/.zsh/aliases/misc.sh
@@ -1,6 +1,5 @@
 alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
-alias weechat="ssh supakuman -t \"tmux attach\""
-alias irc="tmux new-window -n irc \"ssh boxer.freerunningtech.com -t tmux attach\""
+alias irc="tmux new-window -n irc \"ssh supakuman -t 'tmux attach'\""
 alias tms="tmux attach -t"
 alias tm='tmux switch -t "`tmux list-sessions | cut -d':' -f1 | fzy`"'
 alias love=/Applications/love.app/Contents/MacOS/love
