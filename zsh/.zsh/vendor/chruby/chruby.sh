93c0ebf57a313a9407253361166ded379d0fa202
Jared Norman <jared@freerunningtechnologies.com>
2015-05-08 20:23:41 -0700

    Update chruby

--- zsh/.zsh/vendor/chruby/chruby.sh
+++ zsh/.zsh/vendor/chruby/chruby.sh
@@ -11,21 +11,22 @@ function chruby_reset()
 	[[ -z "$RUBY_ROOT" ]] && return
 
 	PATH=":$PATH:"; PATH="${PATH//:$RUBY_ROOT\/bin:/:}"
+	[[ -n "$GEM_ROOT" ]] && PATH="${PATH//:$GEM_ROOT\/bin:/:}"
 
-	if (( $UID != 0 )); then
+	if (( UID != 0 )); then
 		[[ -n "$GEM_HOME" ]] && PATH="${PATH//:$GEM_HOME\/bin:/:}"
-		[[ -n "$GEM_ROOT" ]] && PATH="${PATH//:$GEM_ROOT\/bin:/:}"
 
 		GEM_PATH=":$GEM_PATH:"
 		[[ -n "$GEM_HOME" ]] && GEM_PATH="${GEM_PATH//:$GEM_HOME:/:}"
 		[[ -n "$GEM_ROOT" ]] && GEM_PATH="${GEM_PATH//:$GEM_ROOT:/:}"
 		GEM_PATH="${GEM_PATH#:}"; GEM_PATH="${GEM_PATH%:}"
-		unset GEM_ROOT GEM_HOME
+
+		unset GEM_HOME
 		[[ -z "$GEM_PATH" ]] && unset GEM_PATH
 	fi
 
 	PATH="${PATH#:}"; PATH="${PATH%:}"
-	unset RUBY_ROOT RUBY_ENGINE RUBY_VERSION RUBYOPT
+	unset RUBY_ROOT RUBY_ENGINE RUBY_VERSION RUBYOPT GEM_ROOT
 	hash -r
 }
 
@@ -42,18 +43,21 @@ function chruby_use()
 	export RUBYOPT="$2"
 	export PATH="$RUBY_ROOT/bin:$PATH"
 
-	eval "$("$RUBY_ROOT/bin/ruby" - <<EOF
-puts "export RUBY_ENGINE=#{defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'};"
+	eval "$(RUBYGEMS_GEMDEPS="" "$RUBY_ROOT/bin/ruby" - <<EOF
+puts "export RUBY_ENGINE=#{Object.const_defined?(:RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'};"
 puts "export RUBY_VERSION=#{RUBY_VERSION};"
 begin; require 'rubygems'; puts "export GEM_ROOT=#{Gem.default_dir.inspect};"; rescue LoadError; end
 EOF
 )"
+	export PATH="${GEM_ROOT:+$GEM_ROOT/bin:}$PATH"
 
-	if (( $UID != 0 )); then
+	if (( UID != 0 )); then
 		export GEM_HOME="$HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION"
 		export GEM_PATH="$GEM_HOME${GEM_ROOT:+:$GEM_ROOT}${GEM_PATH:+:$GEM_PATH}"
-		export PATH="$GEM_HOME/bin${GEM_ROOT:+:$GEM_ROOT/bin}:$PATH"
+		export PATH="$GEM_HOME/bin:$PATH"
 	fi
+
+	hash -r
 }
 
 function chruby()
