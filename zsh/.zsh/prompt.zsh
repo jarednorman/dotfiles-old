59a54979aa4430bd89c07e83a328f36e7f1bb992
Jared Norman <jared@freerunningtechnologies.com>
2015-05-16 19:57:15 -0700

    Different prompt colours

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -12,8 +12,8 @@ function parse_git_branch {
 local suspended_jobs='%{$fg_bold[green]%}%(1j.(%j).)%{$reset_color%}'
 local ruby_version='%{$fg_bold[cyan]%}$(chruby | grep "*" | cut -d" " -f3)%{$reset_color%}'
 local root_prompt='%{$fg[red]%}(YOU ARE ROOT)%{$fg_bold[red]%}#'
-local user_prompt='%{$fg[blue]%}>'
-local current_dir='%{$fg[cyan]%}%1d%{$reset_color%}'
+local user_prompt='%{$fg[magenta]%}>'
+local current_dir='%{$fg[green]%}%1d%{$reset_color%}'
 local git_current_branch='%{$fg[blue]%}$(parse_git_branch)%{$reset_color%}'
 
 export PROMPT="${current_dir}${suspended_jobs}%(!.${root_prompt}.${user_prompt}) %{$reset_color%}"
