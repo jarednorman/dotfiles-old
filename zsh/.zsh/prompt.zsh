be0429c9f50eee492f410e116882c446dda7383d
Jared Norman <jared@freerunningtechnologies.com>
2015-05-30 10:26:48 -0700

    Steal and modify prompt from @adammathys

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,21 +1,24 @@
-autoload -U colors && colors
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:git*' formats "%F{blue}%b%f%F{14}[%f%u%c%F{14}]%f "
+zstyle ':vcs_info:*' actionformats "%F{blue}%s%f:%F{cyan}%b%f %F{red}(%a)%f(%u%c) "
+zstyle ':vcs_info:*' stagedstr "%F{green}✓%f"
+zstyle ':vcs_info:*' unstagedstr "%F{red}×%f"
+precmd() { vcs_info }
+
+local current_dir="%F{13}%~%f%b "
+local root_prompt="%F{red}%#%f "
+local user_prompt="%F{cyan}>%f "
+PROMPT='${current_dir}${vcs_info_msg_0_}%(!.${root_prompt}.${user_prompt})'
+
+# I don't know what this does... stolen from Adam. I'll look into it one day. :D
+function zle-line-init zle-keymap-select {
+  local vim_prompt="%K{cyan}%F{white}%B NORMAL %b%f%k"
+  RPROMPT="${${KEYMAP/vicmd/${vim_prompt}}/(main|viins)/}"
+  zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
 
 setopt promptsubst
-
-function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working directory clean" ]] && echo "!"
-}
-function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1%s$(parse_git_dirty))/"
-}
-
-local suspended_jobs='%{$fg_bold[green]%}%(1j.(%j).)%{$reset_color%}'
-local ruby_version='%{$fg_bold[cyan]%}$(chruby | grep "*" | cut -d" " -f3)%{$reset_color%}'
-local root_prompt='%{$fg[red]%}(YOU ARE ROOT)%{$fg_bold[red]%}#'
-local user_prompt='%{$fg[magenta]%}>'
-local current_dir='%{$fg[green]%}%1d%{$reset_color%}'
-local git_current_branch='%{$fg[blue]%}$(parse_git_branch)%{$reset_color%}'
-
-export PROMPT="${current_dir}${suspended_jobs}%(!.${root_prompt}.${user_prompt}) %{$reset_color%}"
-
-export RPROMPT="${ruby_version} ${git_current_branch}"
