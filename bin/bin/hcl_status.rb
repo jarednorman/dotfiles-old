18d5689ac10ec5b52afe3db812b726e193578344
Jared Norman <jared@freerunningtechnologies.com>
2015-09-01 21:54:52 -0700

    Fun tweaks for the new lappy

--- bin/bin/hcl_status.rb
+++ bin/bin/hcl_status.rb
@@ -6,7 +6,7 @@ hcl = HCl::App.new
 entry = HCl::DayEntry.with_timer(hcl.http)
 
 if entry
-  puts "<fc=#fdf6e3,#268bd2> #{entry.project}: #{entry.task} (#{entry.formatted_hours}) </fc>"
+  puts " #{entry.project} (#{entry.task}): #{entry.formatted_hours} "
 else
-  puts '<fc=#fdf6e3,#dc322f> NO TIMER RUNNING </fc>'
+  puts ""
 end
