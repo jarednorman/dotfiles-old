b731137c6afa628a08932ba6577b79a6c9c9bace
Jared Norman <jared@creepywizard.com>
2015-11-08 12:51:37 -0800

    Better font, better ewmh etc support

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -2,22 +2,17 @@ import System.IO
 import XMonad
 import qualified Data.Map as M
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
-import XMonad.Hooks.DynamicLog
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Config.Xfce
 
 myLayoutHook = avoidStruts (noBorders Full ||| Full)
 
 main = do
-  xmonad $ defaultConfig
-           { manageHook         = manageDocks <+> manageHook defaultConfig
-           , terminal           = "urxvt"
+  xmonad $ xfceConfig
+           { terminal           = "urxvt"
            , modMask            = mod4Mask
            , borderWidth        = 4
            , normalBorderColor  = "#fdf6e5"
            , focusedBorderColor = "#eee8d5"
            , layoutHook         = myLayoutHook
-           , handleEventHook    = ewmhDesktopsEventHook
-           , startupHook        = ewmhDesktopsStartup
            }
