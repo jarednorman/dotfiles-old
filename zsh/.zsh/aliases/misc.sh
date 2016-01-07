46688742b6aed89f0c5522653a3a80322443ce7c
Jared Norman <jared@creepywizard.com>
2016-01-06 17:00:25 -0800

    Remove bad old alias

--- zsh/.zsh/aliases/misc.sh
+++ zsh/.zsh/aliases/misc.sh
@@ -2,6 +2,5 @@ alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-
 alias irc="tmux new-window -n irc \"ssh supakuman -t 'tmux attach'\""
 alias tms="tmux attach -t"
 alias tm='tmux switch -t "`tmux list-sessions | cut -d':' -f1 | fzy`"'
-alias love=/Applications/love.app/Contents/MacOS/love
 
 f(){ ag --nocolor -l -g "$1" "${2:-.}"  }
