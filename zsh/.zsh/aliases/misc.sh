23b4103c5dbea7568d7461041cb0b6b6a79349f9
Jared Norman <jared@creepywizard.com>
2016-03-17 22:31:38 -0700

    Add alias for resetting keyboard stuff

--- zsh/.zsh/aliases/misc.sh
+++ zsh/.zsh/aliases/misc.sh
@@ -2,5 +2,6 @@ alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-
 alias irc="tmux new-window -n irc \"ssh supakuman -t 'tmux attach'\""
 alias tms="tmux attach -t"
 alias tm='tmux switch -t "`tmux list-sessions | cut -d':' -f1 | fzy`"'
+alias fix-keyboard="xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' && xset r rate 300 65"
 
 f(){ ag --nocolor -l -g "$1" "${2:-.}"  }
