927d3009bda23fabec6e74d964ec368cb33c967d
Jared Norman <jared@creepywizard.com>
2016-03-19 10:40:11 -0700

    Add ability to toggle avoidstruts

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -7,6 +7,7 @@ import XMonad.Util.EZConfig
 import Graphics.X11.ExtraTypes.XF86
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.EwmhDesktops
 
 myLayoutHook = avoidStruts (noBorders Full ||| Full)
 
@@ -23,21 +24,22 @@ main = do
          , handleEventHook    = ewmhDesktopsEventHook
          , startupHook        = ewmhDesktopsStartup
          , logHook = dynamicLogWithPP $ xmobarPP
-                     { ppOutput = hPutStrLn xmproc
-                     , ppTitle = xmobarColor "#2aa198" ""
-                     , ppCurrent = xmobarColor "#cb4b16" ""
-                     , ppVisible = xmobarColor "#586e75" ""
-                     , ppHidden = xmobarColor "#93a1a1" ""
+                     { ppOutput          = hPutStrLn xmproc
+                     , ppTitle           = xmobarColor "#2aa198" ""
+                     , ppCurrent         = xmobarColor "#cb4b16" ""
+                     , ppVisible         = xmobarColor "#586e75" ""
+                     , ppHidden          = xmobarColor "#93a1a1" ""
                      , ppHiddenNoWindows = xmobarColor "#fdf6e5" ""
-                     , ppLayout = xmobarColor "#859900" ""
-                     , ppSep = " × "
+                     , ppLayout          = xmobarColor "#859900" ""
+                     , ppSep             = " × "
                      }
          }
          `additionalKeys`
-         [ ((0, xF86XK_AudioLowerVolume ), spawn "pactl set-sink-volume 1 -5%")
-         , ((0, xF86XK_AudioRaiseVolume ), spawn "pactl set-sink-volume 1 +5%")
-         , ((0, xF86XK_AudioMute        ), spawn "pactl set-sink-mute 1 toggle")
-         , ((0, xF86XK_MonBrightnessUp  ), spawn "xbacklight +5")
-         , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -5")
-         , ((0, xF86XK_Explorer         ), spawn "xdotool key XF86AudioPlay")
+         [ ((0,        xF86XK_AudioLowerVolume ), spawn "pactl set-sink-volume 1 -5%")
+         , ((0,        xF86XK_AudioRaiseVolume ), spawn "pactl set-sink-volume 1 +5%")
+         , ((0,        xF86XK_AudioMute        ), spawn "pactl set-sink-mute 1 toggle")
+         , ((0,        xF86XK_MonBrightnessUp  ), spawn "xbacklight +5")
+         , ((0,        xF86XK_MonBrightnessDown), spawn "xbacklight -5")
+         , ((0,        xF86XK_Explorer         ), spawn "xdotool key XF86AudioPlay")
+         , ((mod4Mask, xK_b                    ), sendMessage ToggleStruts)
          ]
