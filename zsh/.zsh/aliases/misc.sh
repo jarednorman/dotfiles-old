ece530d665dd7715842b314fe675168d92c345f4
Jared Norman <jared@creepywizard.com>
2016-05-30 15:00:03 -0700

    Thanks @adammathys

--- zsh/.zsh/aliases/misc.sh
+++ zsh/.zsh/aliases/misc.sh
@@ -5,3 +5,6 @@ alias tm='tmux switch -t "`tmux list-sessions | cut -d':' -f1 | fzy`"'
 alias fix-keyboard="xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' && xset r rate 300 65"
 
 f(){ ag --nocolor -l -g "$1" "${2:-.}"  }
+
+# Find and replace using sed and silver!
+replace(){ ag -l --nocolor "$1" "${3:-.}" | xargs sed -i -e "s/$1/$2/g" }
