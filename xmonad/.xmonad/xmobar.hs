c653624f6fba10720a017a6dd93d4c09db58fdb2
Jared Norman <jared@creepywizard.com>
2016-07-16 09:49:05 -0700

    Correct xmobar height for the laptop screen

--- xmonad/.xmonad/xmobar.hs
+++ xmonad/.xmonad/xmobar.hs
@@ -2,7 +2,7 @@ Config { font = "xft:Office Code Pro:medium:size=10"
        , border = NoBorder
        , bgColor = "#073642"
        , fgColor = "#586e75"
-       , position = BottomSize L 100 24
+       , position = BottomSize L 100 26
        , commands = [ Run StdinReader
                     , Run Com "/home/jared/bin/timer_status.sh" [] "timer" 50
                     , Run Date "<fc=#839496>%d %B %Y</fc>, <fc=#93a1a1>%I:%M %p</fc>" "date" 10
