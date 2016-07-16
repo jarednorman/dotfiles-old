7c7855fb76f9fed2b459be8588eaa42e90177b4e
Jared Norman <jared@creepywizard.com>
2016-07-15 18:48:45 -0700

    Fix up status line

--- xmonad/.xmonad/xmobar.hs
+++ xmonad/.xmonad/xmobar.hs
@@ -1,8 +1,8 @@
-Config { font = "xft:Office Code Pro:medium:pixelsize=13"
+Config { font = "xft:Office Code Pro:medium:size=10"
        , border = NoBorder
        , bgColor = "#073642"
        , fgColor = "#586e75"
-       , position = BottomSize L 100 29
+       , position = BottomSize L 100 24
        , commands = [ Run StdinReader
                     , Run Com "/home/jared/bin/timer_status.sh" [] "timer" 50
                     , Run Date "<fc=#839496>%d %B %Y</fc>, <fc=#93a1a1>%I:%M %p</fc>" "date" 10
