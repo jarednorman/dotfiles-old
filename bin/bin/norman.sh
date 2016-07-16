acfe87e3ff7d3adf2727993bf9491baf44d6c159
Jared Norman <jared@creepywizard.com>
2016-07-15 17:53:33 -0700

    Delete unused scripts

--- bin/bin/norman.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-set -vx
-setxkbmap -v norman
-xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
--- bin/bin/us.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-setxkbmap -v us
-xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
