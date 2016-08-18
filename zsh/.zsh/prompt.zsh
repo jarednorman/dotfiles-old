6ad5f3c35ce0f523c2bf6d346228c0cd8ad0ea22
Jared Norman <jared@creepywizard.com>
2016-08-17 18:41:02 -0700

    Clean up some prompt crap

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,18 +1,18 @@
 # Add this to your .oh-my-zsh theme if you're using those, or directly to your zsh theme :)
 
 # Colors vary depending on time lapsed.
-ZSH_THEME_GIT_TIME_SINCE_COMMIT_SHORT="%F{2}"
-ZSH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM="%F{3}"
-ZSH_THEME_GIT_TIME_SINCE_COMMIT_LONG="%F{1}"
-ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL="%F{10}"
+ZSH_THEME_GIT_TIME_SINCE_COMMIT_SHORT="%K{2}%F{8}"
+ZSH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM="%K{3}%F{8}"
+ZSH_THEME_GIT_TIME_SINCE_COMMIT_LONG="%K{9}%F{8}%B"
+ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL="%K{14}%F{8}"
 
 #Customized git status, oh-my-zsh currently does not allow render dirty status before branch
-git_custom_status() {
-  local cb=$(current_branch)
-  if [ -n "$cb" ]; then
-    echo "$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_PREFIX$(current_branch)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-  fi
-}
+# git_custom_status() {
+#   local cb=$(current_branch)
+#   if [ -n "$cb" ]; then
+#     echo "$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_PREFIX$(current_branch)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+#   fi
+# }
 
 # Determine the time since last commit. If branch is clean,
 # use a neutral color, otherwise colors will vary according to time.
@@ -35,9 +35,9 @@ function git_time_since_commit() {
       SUB_MINUTES=$((MINUTES % 60))
 
       if [[ -n $(git status -s 2> /dev/null) ]]; then
-        if [ "$MINUTES" -gt 15 ]; then
+        if [ "$MINUTES" -gt 25 ]; then
           COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_LONG"
-        elif [ "$MINUTES" -gt 5 ]; then
+        elif [ "$MINUTES" -gt 10 ]; then
           COLOR="$ZSH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM"
         else
           COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_SHORT"
@@ -47,11 +47,11 @@ function git_time_since_commit() {
       fi
 
       if [ "$HOURS" -gt 24 ]; then
-        echo "$COLOR${DAYS}d${SUB_HOURS}h${SUB_MINUTES}m%{$reset_color%}"
+        echo "$COLOR ${DAYS}d${SUB_HOURS}h${SUB_MINUTES}m %{$reset_color%}%b"
       elif [ "$MINUTES" -gt 60 ]; then
-        echo "$COLOR${HOURS}h${SUB_MINUTES}m%{$reset_color%}"
+        echo "$COLOR ${HOURS}h${SUB_MINUTES}m %{$reset_color%}%b"
       else
-        echo "$COLOR${MINUTES}m%{$reset_color%}"
+        echo "$COLOR ${MINUTES}m %{$reset_color%}%b"
       fi
     fi
   fi
@@ -62,16 +62,16 @@ function git_time_since_commit() {
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:git*' formats "%F{4}%b%f%u%c%f "
-zstyle ':vcs_info:*' stagedstr "%F{2}+"
-zstyle ':vcs_info:*' unstagedstr "%F{1}×"
+zstyle ':vcs_info:git*' formats "%F{8}%K{4} %b%f%u%c%f "
+zstyle ':vcs_info:*' stagedstr "%F{8}+"
+zstyle ':vcs_info:*' unstagedstr "%F{8}×"
 precmd() { vcs_info }
 
 local suspended_jobs="%F{6}%(1j.(%j).)%f"
-local current_dir="%F{14}%~%f%b"
+local current_dir=" %~%f %b"
 local root_prompt="%F{9}%#%f%b "
 local user_prompt="%F{5}$%f%b "
-PROMPT='$(git_time_since_commit) ${vcs_info_msg_0_}%b%F{10}ruby-$RUBY_VERSION%f
-${current_dir}%f%b${suspended_jobs}%(!.${root_prompt}.${user_prompt})'
+PROMPT='${vcs_info_msg_0_}$(git_time_since_commit)%F{8}%K{11}${current_dir}%K{0}%F{10} ruby-$RUBY_VERSION 
+%f%b${suspended_jobs}%(!.${root_prompt}.${user_prompt})'
 
 setopt promptsubst
