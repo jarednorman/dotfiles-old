fdefafe36f11a3c933d116a1bb830e9ff033010f
Jared Norman <jared@creepywizard.com>
2016-08-19 21:30:33 -0700

    Continue cleaning things up

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -1,69 +1,23 @@
-local hostname = require('hostname')
-local isLaptop = hostname() == "baburuman"
-
 -- Standard awesome library
 local awful = require("awful")
 awful.rules = require("awful.rules")
-require("awful.autofocus")
-
--- Widget and layout library
 local wibox = require("wibox")
-
--- Theme handling library
 local beautiful = require("beautiful")
-
--- Notification library
-local naughty = require("naughty")
-local menubar = require("menubar")
-
--- Load Debian menu entries
-require("debian.menu")
-
--- Vicious
 local vicious = require("vicious")
 
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-  naughty.notify({ title = "Oops, there were errors during startup!",
-  text = awesome.startup_errors })
-end
+require("awful.autofocus")
 
--- Handle runtime errors after startup
-do
-  local in_error = false
-  awesome.connect_signal("debug::error", function (err)
-    -- Make sure we don't go into an endless error loop
-    if in_error then return end
-    in_error = true
+require("errorhandling")
 
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, an error happened!",
-                     text = err })
-    in_error = false
-  end)
-end
+local hostname = require('hostname')
+local isLaptop = hostname() == "baburuman"
 
--- Themes define colours, icons, font and wallpapers.
 beautiful.init("~/.config/awesome/theme.lua")
 
--- This is used later as the default terminal and editor to run.
-if isLaptop then
-  terminal = "st -f 'Input:size=13'"
-else
-  terminal = "st -f 'Input:size=14'"
-end
-editor = os.getenv("EDITOR") or "editor"
-editor_cmd = terminal .. " -e " .. editor
+local terminal = "st -f 'Input:size=13'"
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+local modkey = "Mod4"
 
--- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts = {
   awful.layout.suit.max,
   awful.layout.suit.floating,
@@ -75,9 +29,6 @@ tags = {}
 for s = 1, screen.count() do
   tags[s] = awful.tag({ "C", "R", "E", "E", "P", "Y" }, s, layouts[1])
 end
-
--- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 
 -- Create a wibox for each screen and add it
 local mywibox = {}
