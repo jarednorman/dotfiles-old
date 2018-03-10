3d41db5201887c6b877e1c78f9e03b8c105d60b1
Jared Norman <jared@creepywizard.com>
2018-03-10 14:06:30 -0800

    Prompt colours too

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -2,16 +2,15 @@ autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git svn
 zstyle ':vcs_info:git*' check-for-changes true
 zstyle ':vcs_info:git*' check-for-staged-changes true
-zstyle ':vcs_info:*' stagedstr '%F{4}•'
-zstyle ':vcs_info:*' unstagedstr '%F{1}•'
-zstyle ':vcs_info:git*' formats '%F{14}(%F{10}%b%u%c%F{14})'
-zstyle ':vcs_info:git*' actionformats '%F{14}(%F{10}%b%u%c%F{14}|%F{2}%a%F{14})'
+zstyle ':vcs_info:*' stagedstr '%F{2}⊕'
+zstyle ':vcs_info:*' unstagedstr '%F{1}⊘'
+zstyle ':vcs_info:git*' formats '%F{8}[%F{4}%b%u%c%F{8}]'
+zstyle ':vcs_info:git*' actionformats '%F{8}[%F{4}%b%u%c%F{8}|%F{2}%a%F{8}]'
 precmd() { vcs_info }
 
 setopt promptsubst
 
-local root_prompt="%F{9}%#"
-local user_prompt="%F{4}→"
+local root_prompt="%F{1}▲"
+local user_prompt="%(?.%F{15}△.%F{1}▽)"
 
-PROMPT='%F{14}%n %m %F{4}%~${vcs_info_msg_0_}
-%(!.${root_prompt}.${user_prompt})%f%k%b '
+PROMPT='%F{5}%n%F{15}@%F{3}%m%F{15}:%F{6}%1~${vcs_info_msg_0_} %(!.${root_prompt}.${user_prompt})%f%k%b '
